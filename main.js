9d682fbecca7fda576c2f5a524758397e5d86961
RomanMuzychka <muzychkaromanwork@gmail.com>
2023-02-16 19:39:40 +0200

    fix 2

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-'use strict'
 
-function add() {
-	let ol = document.getElementById('as')
-	let li = document.createElement('li')
-	let button = document.createElement('button')
-	li.appendChild(button)
-	li.appendChild(document.createTextNode('a'))
-	ol.appendChild(li)
-}
-function deleteLi() {
-	console.log(this.event.target)
-	this.event.target.parentNode.remove()
-}
 'use strict'
 
 let rt = document.getElementById('rt')
